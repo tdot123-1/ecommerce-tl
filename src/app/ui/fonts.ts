99bfbcb3dd42d9dbcbf738cab95481cd1a54538d
Thomas Kruithof <tkruithof95@gmail.com>
2024-09-25 19:48:54 +0200

    init

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Roboto_Condensed, Montserrat } from "next/font/google";
+
+// body
+export const roboto = Roboto_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+// headers
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
