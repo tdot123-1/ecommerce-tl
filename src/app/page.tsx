8f79acc4fa29c253361e608f7d148e22f7c6e360
Thomas Kruithof <tkruithof95@gmail.com>
2024-09-27 22:06:32 +0200

    structure home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import { montserrat } from "./ui/fonts";
 import { Button } from "@/components/ui/button";
+import {
+  ChefHatIcon,
+  LucideBriefcase,
+  LucideDiamond,
+  ShirtIcon,
+  ShowerHeadIcon,
+  SmileIcon,
+} from "lucide-react";
 
 export default function Home() {
   return (
@@ -19,61 +27,83 @@ export default function Home() {
           />
         </div>
       </section>
+
       <section className="h-screen">
-        <article className="grid grid-cols-4 grid-rows-5 gap-1">
-          <div className="col-span-4">
-            <h1 className={`${montserrat.className} text-3xl font-semibold`}>
-              Easy Shopping
-            </h1>
+        <article className="h-full">
+          <h1 className={`${montserrat.className} text-3xl font-semibold`}>
+            Easy Shopping
+          </h1>
+          <div className="flex justify-evenly items-center h-fit mt-20">
+            {/* image container */}
+            <div className="flex flex-row gap-2">
+              {/* large image */}
+              <div className="w-96">
+                <Image
+                  src="/placeholder.png"
+                  alt="placeholder"
+                  width={2400}
+                  height={2400}
+                />
+              </div>
+              {/* stacked images */}
+              <div className="flex flex-col justify-between">
+                <div className="w-44">
+                  <Image
+                    src="/placeholder.png"
+                    alt="placeholder"
+                    width={2400}
+                    height={2400}
+                  />
+                </div>
+                <div className="w-44">
+                  <Image
+                    src="/placeholder.png"
+                    alt="placeholder"
+                    width={2400}
+                    height={2400}
+                  />
+                </div>
+              </div>
+            </div>
+            {/* text and link */}
+            <div className="flex flex-col items-center gap-7">
+              <p className="w-60 text-zinc-600 dark:text-zinc-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+                exercitationem obcaecati architecto voluptatibus, similique,
+                veniam, libero eum at et vitae rerum provident consectetur
+                voluptates quasi ea natus accusantium est illum?
+              </p>
+              <Button>Link</Button>
+            </div>
           </div>
-          <div className="col-span-2 row-span-3 row-start-2">
-            <Image
-              src="/placeholder.png"
-              alt="placeholder"
-              width={2400}
-              height={2400}
-            />
-          </div>
-          <div className="row-span-2 col-start-3 row-start-2">
-            <Image
-              src="/placeholder.png"
-              alt="placeholder"
-              width={2400}
-              height={2400}
-            />
-          </div>
-          <div className="row-span-2 col-start-3 row-start-4">
-            <Image
-              src="/placeholder.png"
-              alt="placeholder"
-              width={2400}
-              height={2400}
-            />
-          </div>
-          <div className="row-span-3 col-start-4 row-start-2">
-            <p className=" text-zinc-600 dark:text-zinc-400">
+        </article>
+      </section>
+
+      <section className="h-screen">
+        <article>
+          <h1 className={`${montserrat.className} text-3xl font-semibold`}>
+            Browse Categories
+          </h1>
+          <div>
+            {/* category links */}
+            <div className="flex justify-evenly items-center gap-10 mt-20">
+              <div className="p-7 border border-zinc-500 rounded-lg shadow-lg">
+                <ShirtIcon size={32} />
+              </div>
+              <div className="p-7 border border-zinc-500 rounded-lg shadow-lg">
+                <LucideBriefcase size={32} />
+              </div>
+              <div className="p-7 border border-zinc-500 rounded-lg shadow-lg">
+                <SmileIcon size={32} />
+              </div>
+            </div>
+            <p className=" w-60 mx-auto mt-10 text-zinc-600 dark:text-zinc-300">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
               rem ratione, aperiam fuga, voluptatibus alias tempora iusto
               delectus corrupti odit eos quae itaque sit mollitia hic, maxime
               rerum atque. Est?
             </p>
           </div>
-          <div className="col-start-4 row-start-5">
-            <Button>Button</Button>
-          </div>
-        </article>
-      </section>
-      <section className="h-screen">
-        <article>
-          <h1 className={`${montserrat.className} text-3xl font-semibold`}>
-            Browse Categories
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae rem
-            ratione, aperiam fuga, voluptatibus alias tempora iusto delectus
-            corrupti odit eos quae itaque sit mollitia hic, maxime rerum atque.
-            Est?
-          </p>
         </article>
       </section>
       <section className="h-screen">
