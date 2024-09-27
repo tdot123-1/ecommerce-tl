b06a0c92cd96b44f787445db0df8856da1bfe7e2
Thomas Kruithof <tkruithof95@gmail.com>
2024-09-27 20:04:34 +0200

    work on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,94 @@
+import Image from "next/image";
 import { montserrat } from "./ui/fonts";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main>
-      <div className="w-52 mx-auto mt-4">
-        <h1 className={`${montserrat.className} text-3xl font-bold`}>Homepage</h1>
-      </div>
-      <div className="w-52 mx-auto mt-3">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
-          aliquam accusantium quam dicta enim quibusdam magnam non earum minima
-          neque perferendis maiores, voluptas quae? Pariatur eaque voluptatem
-          in. Rem, sunt?
-        </p>
-      </div>
-    </main>
+    <>
+      <section className="flex justify-center items-center gap-6 h-[calc(100vh-80px)]">
+        <div className="w-60">
+          <Image src="/logo4.png" alt="Logo" width={298} height={390} />
+        </div>
+        <div className="hidden md:block w-1/2">
+          <Image
+            src="/hero-placeholder.jpg"
+            alt="Hero"
+            width={1920}
+            height={1280}
+            className="rounded-lg"
+          />
+        </div>
+      </section>
+      <section className="h-screen">
+        <article className="grid grid-cols-4 grid-rows-5 gap-1">
+          <div className="col-span-4">
+            <h1 className={`${montserrat.className} text-3xl font-semibold`}>
+              Easy Shopping
+            </h1>
+          </div>
+          <div className="col-span-2 row-span-3 row-start-2">
+            <Image
+              src="/placeholder.png"
+              alt="placeholder"
+              width={2400}
+              height={2400}
+            />
+          </div>
+          <div className="row-span-2 col-start-3 row-start-2">
+            <Image
+              src="/placeholder.png"
+              alt="placeholder"
+              width={2400}
+              height={2400}
+            />
+          </div>
+          <div className="row-span-2 col-start-3 row-start-4">
+            <Image
+              src="/placeholder.png"
+              alt="placeholder"
+              width={2400}
+              height={2400}
+            />
+          </div>
+          <div className="row-span-3 col-start-4 row-start-2">
+            <p className=" text-zinc-600 dark:text-zinc-400">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae
+              rem ratione, aperiam fuga, voluptatibus alias tempora iusto
+              delectus corrupti odit eos quae itaque sit mollitia hic, maxime
+              rerum atque. Est?
+            </p>
+          </div>
+          <div className="col-start-4 row-start-5">
+            <Button>Button</Button>
+          </div>
+        </article>
+      </section>
+      <section className="h-screen">
+        <article>
+          <h1 className={`${montserrat.className} text-3xl font-semibold`}>
+            Browse Categories
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae rem
+            ratione, aperiam fuga, voluptatibus alias tempora iusto delectus
+            corrupti odit eos quae itaque sit mollitia hic, maxime rerum atque.
+            Est?
+          </p>
+        </article>
+      </section>
+      <section className="h-screen">
+        <article>
+          <h1 className={`${montserrat.className} text-3xl font-semibold`}>
+            All Products
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae rem
+            ratione, aperiam fuga, voluptatibus alias tempora iusto delectus
+            corrupti odit eos quae itaque sit mollitia hic, maxime rerum atque.
+            Est?
+          </p>
+        </article>
+      </section>
+    </>
   );
 }
--- src/app/ui/customer/navbar.tsx
+++ src/app/ui/customer/navbar.tsx
@@ -2,13 +2,11 @@ import Image from "next/image";
 import NavLinks from "./nav-links";
 import ThemeButton from "../theme-button";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import {  ShoppingBasketIcon } from "lucide-react";
 import Basket from "./basket";
 
 const Navbar = () => {
   return (
-    <nav className="bg-zinc-300 flex justify-between items-center px-5 py-3 border-b border-zinc-500 dark:bg-zinc-900">
+    <nav className="sticky top-0 z-10 bg-zinc-300 flex justify-between items-center px-5 py-3 border-b border-zinc-500 dark:bg-zinc-900">
       <div className="w-10">
         <Link href="/">
           <Image src="/logo3.png" alt="Logo" width={214} height={264} />
