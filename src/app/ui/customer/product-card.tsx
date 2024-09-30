71ed315d8a1b9d854b86174a3c83310222f956d7
Thomas Kruithof <tkruithof95@gmail.com>
2024-09-30 19:23:55 +0200

    add product cards

--- src/app/ui/customer/product-card.tsx
+++ src/app/ui/customer/product-card.tsx
@@ -1,11 +1,36 @@
-const ProductCard = () => {
+import Image from "next/image";
+import { montserrat } from "../fonts";
+import Link from "next/link";
+
+interface ProductCardProps {
+  productId: number;
+}
+
+const ProductCard = ({ productId }: ProductCardProps) => {
   return (
-    <div>
-      <h1>Product name</h1>
-      <span>Image</span>
-      <p>Short description?</p>
-      <span>Sizes/rating ?</span>
-      <span>Price</span>
+    <div className="border border-zinc-500 rounded-lg shadow-xl w-full flex flex-col bg-zinc-200 dark:bg-zinc-800 hover:cursor-pointer">
+      <Link href={`products/${productId}`}>
+        <div className="w-fit self-center pt-1 px-1">
+          <Image
+            src="/placeholder.png"
+            alt="placeholder"
+            width={2400}
+            height={2400}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="px-5 py-3">
+          <h2 className={`text-lg font-semibold ${montserrat.className}`}>
+            Product name
+          </h2>
+          <p className="text-sm">
+            Sizes: <span>S / M / L</span>
+          </p>
+          <p className="text-sm">
+            Price: $<span>10.00</span>
+          </p>
+        </div>
+      </Link>
     </div>
   );
 };
