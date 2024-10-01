0d2376d7b9c868e42e89d435fecf5e3a184a8a15
Thomas Kruithof <tkruithof95@gmail.com>
2024-10-01 20:13:43 +0200

    comment out seed file for safety

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,33 +1,37 @@
+// used for seeding db with initial data
+
 // import { db } from "@vercel/postgres";
 // import { products } from "@/lib/placeholder-data";
 
-// export async function GET() {
-//   const client = await db.connect();
+export async function GET() {
+  return Response.json({ message: "Products already seeded" });
 
-//   try {
-//     await client.sql`BEGIN`;
+  // const client = await db.connect();
 
-//     const insertedProducts = await Promise.all(
-//       products.map((product) => {
-//         const sizesString = product.sizes.join(",");
-//         return client.sql`
-//                       INSERT INTO products (name, price, sizes, description, category, image_url)
-//                       VALUES (${product.name}, ${product.price}, ${sizesString}, ${product.description}, ${product.category}, ${product.image_url})
-//                       ON CONFLICT (id) DO NOTHING;
-//                       `;
-//       })
-//     );
-//     await client.sql`COMMIT`;
+  // try {
+  //   await client.sql`BEGIN`;
 
-//     return Response.json({
-//       message: "DB SEED SUCCESFUL",
-//       inserted: insertedProducts.length,
-//     });
-//   } catch (error) {
-//     await client.sql`ROLLBACK`;
-//     console.error("ERROR SEEDING: ", error);
-//     return Response.json({ message: "ERROR SEEDING" }, { status: 500 });
-//   } finally {
-//     await client.release();
-//   }
-// }
+  //   const insertedProducts = await Promise.all(
+  //     products.map((product) => {
+  //       const sizesString = product.sizes.join(",");
+  //       return client.sql`
+  //                     INSERT INTO products (name, price, sizes, description, category, image_url)
+  //                     VALUES (${product.name}, ${product.price}, ${sizesString}, ${product.description}, ${product.category}, ${product.image_url})
+  //                     ON CONFLICT (id) DO NOTHING;
+  //                     `;
+  //     })
+  //   );
+  //   await client.sql`COMMIT`;
+
+  //   return Response.json({
+  //     message: "DB SEED SUCCESFUL",
+  //     inserted: insertedProducts.length,
+  //   });
+  // } catch (error) {
+  //   await client.sql`ROLLBACK`;
+  //   console.error("ERROR SEEDING: ", error);
+  //   return Response.json({ message: "ERROR SEEDING" }, { status: 500 });
+  // } finally {
+  //   await client.release();
+  // }
+}
