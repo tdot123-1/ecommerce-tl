f8a5316a0ea1147be25be9dd56eba30aae4fbf42
Thomas Kruithof <tkruithof95@gmail.com>
2024-11-08 12:46:15 +0100

    add layout to images edit page

--- src/app/ui/dashboard/images/edit-images.tsx
+++ src/app/ui/dashboard/images/edit-images.tsx
@@ -3,6 +3,9 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import ImageUpload from "../forms/components/image-upload";
 import UploadSection from "./upload-section";
+import Link from "next/link";
+import { SearchIcon } from "lucide-react";
+import { montserrat } from "../../fonts";
 
 interface ImageObject {
   image_url: string;
@@ -25,51 +28,73 @@ const EditImages = ({
 }: EditImagesProps) => {
   return (
     <section>
-      <div>
-        <div className="w-20 relative">
+      <div className="flex justify-center items-center p-4 gap-10 border-b border-zinc-300">
+        <div className="w-40 relative">
           <AspectRatio ratio={8 / 9}>
             <Image
               src={image_url}
               alt={name}
               fill
-              sizes="(max-width: 640px) 40vw, 
-                            (max-width: 1024px) 20vw, 
-                            (max-width: 1280px) 10vw, 
-                            8vw"
+              sizes="(max-width: 640px) 50vw, 
+                            (max-width: 1024px) 30vw, 
+                            (max-width: 1280px) 20vw, 
+                            15vw"
               className="rounded-md"
             />
           </AspectRatio>
         </div>
-        <h2>{name}</h2>
+        <h2 className={`${montserrat.className} text-xl font-semibold mt-4`}>{name}</h2>
       </div>
       <div>
-        {images && images[0].image_url ? (
-          images.map((image) => (
-            <div key={image.image_url}>
-              <div className="w-20 relative">
-                <AspectRatio ratio={8 / 9}>
-                  <Image
-                    src={image.image_url}
-                    alt={`additional image ${image.display_order}`}
-                    fill
-                    sizes="(max-width: 640px) 40vw, 
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
+          {images && images[0].image_url ? (
+            images.map((image) => (
+              <div
+                key={image.image_url}
+                className="flex flex-col md:flex-row items-center justify-evenly py-4 px-2 border-2 border-zinc-300 bg-zinc-100 dark:border-zinc-600 dark:bg-zinc-900 rounded-md"
+              >
+                <div className="">
+                  <div className="w-20 relative mx-auto md:mx-0 mb-2">
+                    <AspectRatio ratio={8 / 9}>
+                      <Image
+                        src={image.image_url}
+                        alt={`additional image ${image.display_order}`}
+                        fill
+                        sizes="(max-width: 640px) 40vw, 
                             (max-width: 1024px) 20vw, 
                             (max-width: 1280px) 10vw, 
                             8vw"
-                    className="rounded-md"
-                  />
-                </AspectRatio>
+                        className="rounded-md"
+                      />
+                    </AspectRatio>
+                  </div>
+                  <div className="text-sm">
+                    <Link target="_blank" href={image.image_url} className="text-blue-600 font-semibold">
+                      <div className="flex justify-start items-start gap-1">
+                        Zoom in <SearchIcon size={12} />
+                      </div>
+                    </Link>
+                    <p>
+                      <span className="font-semibold">Display order:</span>{" "}
+                      {image.display_order}
+                    </p>
+                    <p>
+                      <span className="font-semibold">Created at:</span>{" "}
+                      {new Date(image.created_at).toLocaleDateString()}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2 mt-2 md:mt-0">
+                  <Button variant={`default`}>Change Display Order</Button>
+                  <Button variant={`default`}>Set as Primary</Button>
+                  <Button variant={`destructive`}>Delete</Button>
+                </div>
               </div>
-              <p>{`Display order: ${image.display_order}`}</p>
-              <p>{`Created at: ${image.created_at}`}</p>
-              <Button>Change Display Order</Button>
-              <Button>Set as Primary</Button>
-              <Button>Delete</Button>
-            </div>
-          ))
-        ) : (
-          <p>No additional images yet</p>
-        )}
+            ))
+          ) : (
+            <p>No additional images yet</p>
+          )}
+        </div>
         <UploadSection productId={productId} />
       </div>
     </section>
