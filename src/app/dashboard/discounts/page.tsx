805746e29bb56bbf6a05d1c59a8c28d5537aab44
Thomas Kruithof <tkruithof95@gmail.com>
2024-12-06 18:38:05 +0100

    add todos to discount page

--- src/app/dashboard/discounts/page.tsx
+++ src/app/dashboard/discounts/page.tsx
@@ -2,6 +2,11 @@ const Page = () => {
   return (
     <>
       <h1>Discounts page</h1>
+      <p>Overview of coupons and promo codes</p>
+      <p>Create new coupon</p>
+      <p>Create new promo codes</p>
+      <p>Deactivate coupon/promo code</p>
+      <p>Delete coupon/promo code</p>
     </>
   );
 };
