277ee6ad3c5b8e9842ed3a3b73b18948ab1a556d
Thomas Kruithof <tkruithof95@gmail.com>
2025-01-02 19:11:05 +0100

    start on send email pages

---
+++ src/app/dashboard/mailing/send/plaintext/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>Send plaintext email</h1>;
+};
+
+export default Page;
---
+++ src/app/dashboard/mailing/send/template/[templateId]/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>Send template email</h1>;
+};
+
+export default Page;
